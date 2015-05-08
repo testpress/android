0072d4e5ca875ae8551d4c4b048386fa74fabb65
Adaikalaraj D <adaikalaraj@testpress.in>
2015-05-08 12:49:09 +0530

    Highlight answered and review in filter
    -show different color for index in answered and review at filter

--- app/src/main/java/in/testpress/testpress/ui/PanelListAdapter.java
+++ app/src/main/java/in/testpress/testpress/ui/PanelListAdapter.java
@@ -2,6 +2,7 @@ package in.testpress.testpress.ui;
 
 import android.text.Html;
 import android.view.LayoutInflater;
+import android.view.View;
 
 import java.util.List;
 
@@ -30,14 +31,43 @@ public class PanelListAdapter extends AlternatingColorListAdapter<AttemptItem> {
 
     @Override
     protected int[] getChildViewIds() {
-        return new int[]{R.id.question, R.id.question_index};
+        return new int[]{R.id.question_all, R.id.question_marked, R.id.question_index_all, R.id.question_index_marked,
+        R.id.question_answered, R.id.question_index_answered};
     }
 
     @Override
     protected void update(final int position, final AttemptItem item) {
         String question = Html.fromHtml(item.getAttemptQuestion().getQuestionHtml()).toString();
-        setText(0, trim(question, 0, question.length()));
-        setNumber(1, item.getIndex());
+        try {
+            if(item.getReview() || item.getCurrentReview()) {
+                updater.view.findViewById(R.id.all_question).setVisibility(View.GONE);
+                updater.view.findViewById(R.id.answered_question).setVisibility(View.GONE);
+                updater.view.findViewById(R.id.marked_question).setVisibility(View.VISIBLE);
+                setNumber(3, item.getIndex());
+                setText(1, trim(question, 0, question.length()));
+            }
+            else setAnswer(item, question);
+        }
+        catch (Exception e) {
+            setAnswer(item, question);
+        }
+    }
+
+    void setAnswer(AttemptItem item, String question) {
+        if(!item.getSelectedAnswers().isEmpty() || !item.getSavedAnswers().isEmpty()) {
+            updater.view.findViewById(R.id.marked_question).setVisibility(View.GONE);
+            updater.view.findViewById(R.id.answered_question).setVisibility(View.VISIBLE);
+            updater.view.findViewById(R.id.all_question).setVisibility(View.GONE);
+            setNumber(5, item.getIndex());
+            setText(4, trim(question, 0, question.length()));
+        }
+        else {
+            updater.view.findViewById(R.id.marked_question).setVisibility(View.GONE);
+            updater.view.findViewById(R.id.answered_question).setVisibility(View.GONE);
+            updater.view.findViewById(R.id.all_question).setVisibility(View.VISIBLE);
+            setNumber(2, item.getIndex());
+            setText(0, trim(question, 0, question.length()));
+        }
     }
 
     public static CharSequence trim(CharSequence s, int start, int end) {

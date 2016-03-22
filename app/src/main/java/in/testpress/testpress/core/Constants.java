4e3f86ace1ec33186624a55f738dd95f0483045b
Bharath <bharath@testpress.in>
2016-03-22 11:12:45 +0530

    Logo changes for PositiveMedgPG.

--- app/src/main/java/in/testpress/testpress/core/Constants.java
+++ app/src/main/java/in/testpress/testpress/core/Constants.java
@@ -16,7 +16,7 @@ public final class Constants {
         /**
          * Account name
          */
-        public static final String TESTPRESS_ACCOUNT_NAME = "testpress";
+        public static final String TESTPRESS_ACCOUNT_NAME = "positivemedpg";
 
         /**
          * Account type id
@@ -41,7 +41,7 @@ public final class Constants {
         /**
          * Base URL for all requests
          */
-        public static final String URL_BASE = "http://demo.testpress.in";
+        public static final String URL_BASE = "http://positivemedpg.testpress.in";
 
         /**
          * Check Update url

60a9c30ff1df4c88514525dba57563e7c183a3cc
vignesh-s <vignesh@testpress.in>
2017-03-13 14:18:27 +0530

    Change base url & facebook app id
    Changed the base url to "prime.pgtestseries.com".
    Changed facebook app id.

--- app/src/main/java/in/testpress/testpress/core/Constants.java
+++ app/src/main/java/in/testpress/testpress/core/Constants.java
@@ -36,7 +36,7 @@ public final class Constants {
         /**
          * Base URL for all requests
          */
-        public static final String URL_BASE = "http://pgtestseries.testpress.in";
+        public static final String URL_BASE = "http://prime.pgtestseries.com";
 
         /**
          * Check Update url

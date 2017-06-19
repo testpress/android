87d0cd5e4b8305d1eafe2b36de1a1823bd7eeee0
vignesh-s <vignesh@testpress.in>
2017-06-19 11:40:47 +0530

    Customise for smartjoint

--- app/src/main/java/in/testpress/testpress/core/Constants.java
+++ app/src/main/java/in/testpress/testpress/core/Constants.java
@@ -16,7 +16,7 @@ public final class Constants {
         /**
          * Account name
          */
-        public static final String TESTPRESS_ACCOUNT_NAME = "testpress";
+        public static final String TESTPRESS_ACCOUNT_NAME = "smartjoint";
 
         /**
          * Account type id
@@ -36,7 +36,7 @@ public final class Constants {
         /**
          * Base URL for all requests
          */
-        public static final String URL_BASE = "http://sandbox.testpress.in";
+        public static final String URL_BASE = "http://smartjoint.testpress.in";
 
         /**
          * Check Update url

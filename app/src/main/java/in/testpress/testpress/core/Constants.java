e0e3af388da12aa04eae118c80337936fff91d06
Mohammad Umair <umair@testpress.in>
2018-01-09 12:49:58 +0530

    Changed http to https

--- app/src/main/java/in/testpress/testpress/core/Constants.java
+++ app/src/main/java/in/testpress/testpress/core/Constants.java
@@ -36,7 +36,7 @@ public final class Constants {
         /**
          * Base URL for all requests
          */
-        public static final String URL_BASE = "http://test.pcmbtoday.com";
+        public static final String URL_BASE = "https://test.pcmbtoday.com";
 
         /**
          * Check Update url

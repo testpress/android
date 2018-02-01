cd907f6b1dcc2584a7c5c992d09ac016fe65fa3c
Mohammad Umair <umair@testpress.in>
2018-02-01 17:57:10 +0530

    Change BASE_URL

--- app/src/main/java/in/testpress/testpress/core/Constants.java
+++ app/src/main/java/in/testpress/testpress/core/Constants.java
@@ -36,7 +36,7 @@ public final class Constants {
         /**
          * Base URL for all requests
          */
-        public static final String URL_BASE = "https://venperacademy.testpress.in";
+        public static final String URL_BASE = "https://learn.venperacademy.com/";
 
         /**
          * Check Update url

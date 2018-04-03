a6fe8bbfe4fa1eab5eedae66504e30a68ab7f1e5
vignesh-s <vignesh@testpress.in>
2018-04-03 19:56:07 +0530

    Support deep linking whitelabeled extel academy url

--- app/src/main/java/in/testpress/testpress/core/Constants.java
+++ app/src/main/java/in/testpress/testpress/core/Constants.java
@@ -36,7 +36,7 @@ public final class Constants {
         /**
          * Base URL for all requests
          */
-        public static final String URL_BASE = "https://class.extelacademy.com/";
+        public static final String URL_BASE = "https://extelacademy.testpress.in/";
 
         /**
          * Check Update url

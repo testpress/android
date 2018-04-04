959ad2d84b8a35e5b088581973a6a32aab66c15b
vignesh-s <vignesh@testpress.in>
2018-04-04 12:48:44 +0530

    Use testpress subdomain & Support deeplinking whitelabeled url of mtg

--- app/src/main/java/in/testpress/testpress/core/Constants.java
+++ app/src/main/java/in/testpress/testpress/core/Constants.java
@@ -36,7 +36,7 @@ public final class Constants {
         /**
          * Base URL for all requests
          */
-        public static final String URL_BASE = "https://test.pcmbtoday.com";
+        public static final String URL_BASE = "https://mtg.testpress.in";
 
         /**
          * Check Update url

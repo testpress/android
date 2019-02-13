ba1ff18c4959534a3cf1cb72c3949fb1bd2b3da3
Akshay Kumar <akshaykumar@testpress.in>
2019-02-13 13:40:23 +0530

    Code cleanup

--- app/src/main/java/in/testpress/testpress/models/TestpressApiErrorResponse.java
+++ app/src/main/java/in/testpress/testpress/models/TestpressApiErrorResponse.java
@@ -5,18 +5,14 @@ public class TestpressApiErrorResponse {
     private String detail;
 
     /**
-     *
-     * @return
-     * The detail
+     * @return The detail
      */
     public String getDetail() {
         return detail;
     }
 
     /**
-     *
-     * @param detail
-     * The detail
+     * @param detail The detail
      */
     public void setDetail(String detail) {
         this.detail = detail;

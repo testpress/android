572036938b7584fe19ae90cb89e56bf515afc3c4
Karthik <karthik@testpress.in>
2019-07-11 08:35:13 +0530

    Add babapedia 2020

--- app/src/main/java/in/testpress/testpress/ui/CourseListAdapter.java
+++ app/src/main/java/in/testpress/testpress/ui/CourseListAdapter.java
@@ -28,6 +28,7 @@ class CourseListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>  {
 
     private static final int[][] webLinks = {
             { R.string.babapedia , R.drawable.global_learning, R.string.babapedia_url },
+            { R.string.babapedia_2020 , R.drawable.book, R.string.babapedia_url_2020 },
             { R.string.ilp_2019 , R.drawable.notes, R.string.ilp_url },
             { R.string.ilp_2020, R.drawable.paper_airplane, R.string.ilp_url_2020}
     };

0e5e36125aa272385cee3bce5381a9ebfea17568
Karthik <karthik@testpress.in>
2019-09-11 16:48:37 +0530

    Fix testpressSession cannot be null error on logout
    clearActiveSession method is already written in invalidateAuthToken
    method inside logout function and again it was repeated, this causes to
    clear already cleared session which raises error.

--- app/src/main/java/in/testpress/testpress/TestpressServiceProvider.java
+++ app/src/main/java/in/testpress/testpress/TestpressServiceProvider.java
@@ -112,7 +112,6 @@ public class TestpressServiceProvider {
         preferences.edit().putBoolean(GCMPreference.SENT_TOKEN_TO_SERVER, false).apply();
         CommonUtils.registerDevice(activity, testpressService, serviceProvider);
         TestpressApplication.clearDatabase(activity);
-        TestpressSdk.clearActiveSession(activity);
         TestpressSDKDatabase.clearDatabase(activity);
         logoutService.logout(new Runnable() {
             @Override

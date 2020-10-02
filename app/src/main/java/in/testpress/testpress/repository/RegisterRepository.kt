677804d63ae13246f1525f4d4e14e1589c3180c9
Sangeetha <sangeetha@testpress.in>
2020-10-02 09:59:02 +0530

    Refactor Register Repository

--- app/src/main/java/in/testpress/testpress/repository/RegisterRepository.kt
+++ app/src/main/java/in/testpress/testpress/repository/RegisterRepository.kt
@@ -1,35 +1,47 @@
 package `in`.testpress.testpress.repository
 
+import `in`.testpress.testpress.core.Resource
 import `in`.testpress.testpress.core.TestpressService
+import `in`.testpress.testpress.models.RegistrationSuccessResponse
 import `in`.testpress.testpress.util.SafeAsyncTask
 import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 open class RegisterRepository(val testpressService: TestpressService) {
 
-    var result = MutableLiveData<Boolean>()
-    var exception: Exception? = null
+    var result = MutableLiveData<Resource<RegistrationSuccessResponse>>()
+
+    var registrationSuccessResponse: RegistrationSuccessResponse? = null
 
     fun register(userDetails: HashMap<String, String>) {
+
         object : SafeAsyncTask<Boolean>() {
             override fun call(): Boolean {
-                testpressService.register(userDetails["username"],
-                        userDetails["email"],
-                        userDetails["password"],
-                        userDetails["phone"],
-                        userDetails["country_code"]
-                )
+                try {
+                    registrationSuccessResponse = testpressService.register(userDetails["username"],
+                            userDetails["email"],
+                            userDetails["password"],
+                            userDetails["phone"],
+                            userDetails["country_code"]
+                    )
+                } catch (e: Exception) {
+                    CoroutineScope(Dispatchers.Main).launch {
+                        result.postValue(Resource.error(e, null))
+                    }
+                }
                 return true
             }
 
-            override fun onException(e: Exception?) {
+            override fun onException(e: Exception) {
                 super.onException(e)
-                exception = e
-                result.postValue(false)
+                result.value = (Resource.error(e, null))
             }
 
             override fun onSuccess(authSuccess: Boolean?) {
                 super.onSuccess(authSuccess)
-                result.postValue(true)
+                result.postValue(Resource.success(registrationSuccessResponse))
             }
         }.execute()
     }

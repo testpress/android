3b55d86c44c0f4c5e1c131d46f32581d9b5b5694
Sangeetha <sangeetha@testpress.in>
2020-11-25 14:31:27 +0530

    Release version 1.10.7

--- app/src/main/java/in/testpress/testpress/ui/MainActivity.java
+++ app/src/main/java/in/testpress/testpress/ui/MainActivity.java
@@ -308,6 +308,8 @@ public class MainActivity extends TestpressFragmentActivity {
             if (mInstituteSettings.getForumEnabled()) {
                 addMenuItem(R.string.discussions, R.drawable.chat_icon, new ForumListFragment());
             }
+            DownloadsFragment downloadsFragment = new DownloadsFragment();
+            addMenuItem(R.string.downloads, R.drawable.ic_downloads, downloadsFragment);
         } else {
             grid.setVisibility(View.GONE);
         }

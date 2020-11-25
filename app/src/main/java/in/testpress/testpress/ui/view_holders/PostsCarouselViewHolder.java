847b7c1f4a219844c0edfd54a5be03c1948f9cc0
Sangeetha <sangeetha@testpress.in>
2020-11-25 12:53:46 +0530

    Revert Add SeeAll for Post in Dashboard

---
+++ app/src/main/java/in/testpress/testpress/ui/view_holders/PostsCarouselViewHolder.java
@@ -0,0 +1,35 @@
+package in.testpress.testpress.ui.view_holders;
+
+import android.content.Context;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import android.view.View;
+import in.testpress.testpress.R;
+import in.testpress.testpress.models.pojo.DashboardResponse;
+import in.testpress.testpress.models.pojo.DashboardSection;
+import in.testpress.testpress.ui.adapters.PostCarouselAdapter;
+
+public class PostsCarouselViewHolder extends BaseCarouselViewHolder {
+
+    public PostsCarouselViewHolder(View itemView, Context context) {
+        super(itemView, context);
+    }
+
+    public void display(DashboardResponse response, Context context) {
+        DashboardSection section = response.getAvailableSections().get(getAdapterPosition());
+        PostCarouselAdapter adapter = new PostCarouselAdapter(response, section, context);
+
+        if (section.getItems().size() > 8) {
+            recyclerView.setLayoutManager(new GridLayoutManager(context, 2, GridLayoutManager.HORIZONTAL, false));
+        } else {
+            recyclerView.setLayoutManager(new LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false));
+        }
+        recyclerView.setAdapter(adapter);
+        title.setText(section.getDisplayName());
+        title.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_news_black, 0, 0, 0);
+
+        if (section.getItems().size() > 2) {
+            showPageIndicator();
+        }
+    }
+}

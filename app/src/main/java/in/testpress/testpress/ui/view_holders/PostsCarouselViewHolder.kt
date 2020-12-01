2fcca488560d541b81ea306931f6d2a5bc83ceaa
Sangeetha <sangeetha@testpress.in>
2020-12-01 19:22:06 +0530

    Add See all button for posts in dashboard

--- app/src/main/java/in/testpress/testpress/ui/view_holders/PostsCarouselViewHolder.kt
+++ app/src/main/java/in/testpress/testpress/ui/view_holders/PostsCarouselViewHolder.kt
@@ -3,13 +3,18 @@ package `in`.testpress.testpress.ui.view_holders
 import `in`.testpress.testpress.R
 import `in`.testpress.testpress.models.pojo.DashboardResponse
 import `in`.testpress.testpress.models.pojo.DashboardSection
+import `in`.testpress.testpress.ui.PostsListActivity
 import `in`.testpress.testpress.ui.adapters.PostCarouselAdapter
 import android.content.Context
+import android.content.Intent
 import android.view.View
+import android.widget.TextView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 
 class PostsCarouselViewHolder(itemView: View, context: Context): BaseCarouselViewHolder(itemView, context) {
+
+    private var seeAll: TextView = itemView.findViewById(R.id.see_all)
 
     fun display(response: DashboardResponse, context: Context) {
         val section = response.availableSections[adapterPosition]
@@ -18,6 +23,7 @@ class PostsCarouselViewHolder(itemView: View, context: Context): BaseCarouselVie
         if (section.items.size > 2) {
             showPageIndicator()
         }
+        initOnClickListeners(context)
     }
 
     private fun initRecyclerView(section: DashboardSection, context: Context, response: DashboardResponse) {
@@ -33,5 +39,13 @@ class PostsCarouselViewHolder(itemView: View, context: Context): BaseCarouselVie
     private fun displayTitle(displayName: String) {
         title.text = displayName
         title.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_news_black, 0, 0, 0)
+    }
+
+    private fun initOnClickListeners(context: Context) {
+        seeAll.visibility = View.VISIBLE
+        seeAll.setOnClickListener {
+            val intent = Intent(context, PostsListActivity::class.java)
+            context.startActivity(intent)
+        }
     }
 }

80e756b9be2977fcf3c3aa2174a61d17991e5b48
hari-testpress <hari@testpress.in>
2022-03-31 11:44:03 +0530

    Generate app files

--- fastlane/actions/get_app_config.rb
+++ fastlane/actions/get_app_config.rb
@@ -9,6 +9,10 @@ module Fastlane
         uri = URI('https://hari.in.ngrok.io/api/android/')
         res = Net::HTTP.get_response(uri)
         puts "#{res.code} , #{res.message}"
+        puts "-------------------------------------------"
+        uri = URI('https://reqres.in/api/users/')
+        res1 = Net::HTTP.get_response(uri)
+        puts "#{res1.code} , #{res1.message}"
         return res.body if res.is_a?(Net::HTTPSuccess)
       end
     end

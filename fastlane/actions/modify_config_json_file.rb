5c5b69807f21145bd4109e14892a68cbfbc17b0c
hari-testpress <hari@testpress.in>
2022-05-16 16:02:57 +0530

    Refactor the fastlane view

--- fastlane/actions/modify_config_json_file.rb
+++ fastlane/actions/modify_config_json_file.rb
@@ -6,8 +6,22 @@ module Fastlane
   module Actions
     class ModifyConfigJsonFileAction < Action
       def self.run(params)     
-        fields_to_change = ["allow_anonymous_user","package_name", "app_name", "primary_color", "server_client_id","facebook_app_id", 
-                            "display_username_on_video", "white_labeled_host_url", "is_growth_hacks_enabled", "show_pdf_vertically", "testpress_site_subdomain", "secondary_color","tertiary_color", "zoom_enabled"]
+        fields_to_change = [
+          "allow_anonymous_user",
+          "package_name", 
+          "app_name", 
+          "primary_color", 
+          "server_client_id",
+          "facebook_app_id",          
+          "display_username_on_video", 
+          "white_labeled_host_url", 
+          "is_growth_hacks_enabled", 
+          "show_pdf_vertically", 
+          "testpress_site_subdomain", 
+          "secondary_color",
+          "tertiary_color", 
+          "zoom_enabled"
+        ]
         replace_fields_value(fields_to_change, params[:config_json])
       end
 

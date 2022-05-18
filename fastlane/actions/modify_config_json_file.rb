7a9f629c4fdb5e6dd767c1798596c58dfac4fafb
hari-testpress <hari@testpress.in>
2022-05-18 14:02:20 +0530

    Test Timing

--- fastlane/actions/modify_config_json_file.rb
+++ fastlane/actions/modify_config_json_file.rb
@@ -26,7 +26,8 @@ module Fastlane
       end
 
       def self.replace_fields_value(fields, data)
-          file = File.read("./app/src/main/assets/config.json")
+          path = "./app/src/main/assets/config.json"
+          file = File.read(path)
           config_json = JSON.parse(file)
           app_config_json = JSON.parse(data)
           config_json.each{|key, value| config_json[key] = app_config_json[key] if app_config_json.key?(key)}

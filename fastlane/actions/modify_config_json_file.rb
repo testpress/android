fa74f0fa86d24e4c5a62f6558f4629973038087b
hari-testpress <hari@testpress.in>
2022-05-19 09:18:09 +0530

    Refactor the code

--- fastlane/actions/modify_config_json_file.rb
+++ fastlane/actions/modify_config_json_file.rb
@@ -26,13 +26,14 @@ module Fastlane
       end
 
       def self.replace_fields_value(fields, data)
-          file = File.read("./app/src/main/assets/config.json")
+          path = "./app/src/main/assets/config.json"
+          file = File.read(path)
           config_json = JSON.parse(file)
           app_config_json = JSON.parse(data)
-          config_json.each{|key, value| config_json[key] = app_config_json[key] if app_config_json.key?(key)}
+          config_json.each{|key, value| config_json[key] = app_config_json[key] if app_config_json.key?(key) && fields.include?(key)}
           File.open(path,"w") do |f|
             f.puts JSON.pretty_generate(config_json)
-          end       
+          end  
       end
       def self.available_options
         [

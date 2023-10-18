e9494b2fda4e5736973556e92bde956616e4d029
PruthiviRaj27 <pruthiviraj@testpress.in>
2023-10-18 16:05:36 +0530

    refcator

--- app/src/main/java/in/testpress/testpress/util/InstituteName.kt
+++
@@ -1,9 +0,0 @@
-package `in`.testpress.testpress.util
-
-import android.content.Context
-
-object InstituteName {
-
-    fun isEPratibhaApp(context: Context) = context.packageName == "net.epratibha.www"
-
-}

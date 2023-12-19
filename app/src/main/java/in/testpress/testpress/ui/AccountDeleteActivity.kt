43de2b331e1410d033f147e7b3eaa0af66323127
PruthiviRaj27 <pruthiviraj@testpress.in>
2023-12-19 10:46:37 +0530

    fix

--- app/src/main/java/in/testpress/testpress/ui/AccountDeleteActivity.kt
+++ app/src/main/java/in/testpress/testpress/ui/AccountDeleteActivity.kt
@@ -1,36 +1,45 @@
 package `in`.testpress.testpress.ui
 
+import `in`.testpress.testpress.Injector
 import `in`.testpress.testpress.TestpressServiceProvider
 import `in`.testpress.testpress.authenticator.LogoutService
 import `in`.testpress.testpress.core.TestpressService
 import `in`.testpress.ui.AbstractWebViewActivity
 import `in`.testpress.util.BaseJavaScriptInterface
-import android.util.Log
+import android.os.Bundle
 import android.webkit.JavascriptInterface
 import javax.inject.Inject
 
-class AccountDeleteActivity: AbstractWebViewActivity() {
+class AccountDeleteActivity : AbstractWebViewActivity() {
 
-    @Inject lateinit var serviceProvider: TestpressServiceProvider
-    @Inject lateinit var testpressService: TestpressService
-    @Inject lateinit var logoutService: LogoutService
+    @Inject
+    lateinit var serviceProvider: TestpressServiceProvider
+    @Inject
+    lateinit var testpressService: TestpressService
+    @Inject
+    lateinit var logoutService: LogoutService
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Injector.inject(this)
+    }
 
     override fun onWebViewInitializationSuccess() {
-        webViewFragment.addJavascriptInterface(JavaScriptInterface(this),"AndroidInterface")
+        webViewFragment.addJavascriptInterface(JavaScriptInterface(), "AndroidInterface")
     }
 
-    fun logout() {
-        serviceProvider.logout(this, testpressService, serviceProvider, logoutService)
-    }
+    inner class JavaScriptInterface : BaseJavaScriptInterface(this) {
 
-}
+        @JavascriptInterface
+        fun onAccountDeletionSuccess() {
+            serviceProvider.logout(
+                this@AccountDeleteActivity,
+                testpressService,
+                serviceProvider,
+                logoutService
+            )
+        }
 
-class JavaScriptInterface(val activity: AccountDeleteActivity): BaseJavaScriptInterface(activity) {
-
-    @JavascriptInterface
-    fun onAccountDeletionSuccess() {
-        Log.d("TAG", "onAccountDeletionSuccess: ")
-        activity.logout()
     }
 
 }

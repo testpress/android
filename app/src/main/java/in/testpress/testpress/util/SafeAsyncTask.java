4bb18d8b44ca8a875c04d058a863f399d430a220
PruthiviRaj27 <pruthiviraj@testpress.in>
2025-05-21 16:36:28 +0530

    fix

--- app/src/main/java/in/testpress/testpress/util/SafeAsyncTask.java
+++ app/src/main/java/in/testpress/testpress/util/SafeAsyncTask.java
@@ -183,6 +183,7 @@ public abstract class SafeAsyncTask<ResultT> implements Callable<ResultT> {
                     doException(e);
                 } catch (Exception f) {
                     // logged but ignored
+                    Ln.e(f);
                 }
 
             } catch (final Throwable t) {
@@ -190,6 +191,7 @@ public abstract class SafeAsyncTask<ResultT> implements Callable<ResultT> {
                     doThrowable(t);
                 } catch (Exception f) {
                     // logged but ignored
+                    Ln.e(f);
                 }
             } finally {
                 doFinally();

6061318e60c8de101fefc9f052784cf5edb7c02f
pruthiviraj <pruthiviraj@testpress.in>
2025-07-21 16:37:12 +0530

    fix

---
+++ app/src/main/java/in/testpress/testpress/util/UIUtils.kt
@@ -0,0 +1,20 @@
+package `in`.testpress.testpress.util
+
+import `in`.testpress.testpress.R
+import `in`.testpress.testpress.models.InstituteSettings
+
+fun getMenuItemName(titleResId: Int, instituteSettings: InstituteSettings): String {
+    return when (titleResId) {
+        R.string.dashboard -> Strings.toString(instituteSettings.dashboardLabel)
+        R.string.leaderboard -> Strings.toString(instituteSettings.leaderboardLabel)
+        R.string.bookmarks -> Strings.toString(instituteSettings.bookmarksLabel)
+        R.string.documents -> Strings.toString(instituteSettings.documentsLabel)
+        R.string.store -> Strings.toString(instituteSettings.storeLabel)
+        R.string.posts -> Strings.toString(instituteSettings.postsLabel)
+        R.string.learn -> Strings.toString(instituteSettings.learnLabel)
+        R.string.label_username -> Strings.toString(instituteSettings.loginLabel)
+        R.string.label_password -> Strings.toString(instituteSettings.loginPasswordLabel)
+        R.string.discussions -> Strings.toString(instituteSettings.forumLabel)
+        else -> ""
+    }
+}

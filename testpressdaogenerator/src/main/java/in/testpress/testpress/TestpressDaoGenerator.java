556ea57baca74f4bb03fb365fe106e355267b8f7
Harish V <harishv@testpress.in>
2026-07-03 15:32:41 +0530

    feat: Add custom forgot password URL to institute settings (#508)

--- testpressdaogenerator/src/main/java/in/testpress/testpress/TestpressDaoGenerator.java
+++ testpressdaogenerator/src/main/java/in/testpress/testpress/TestpressDaoGenerator.java
@@ -7,7 +7,7 @@ import de.greenrobot.daogenerator.Schema;
 
 public class TestpressDaoGenerator {
     public static void main(String args[]) throws Exception {
-        Schema schema = new Schema(47, "in.testpress.testpress.models");
+        Schema schema = new Schema(48, "in.testpress.testpress.models");
 
         Entity post = schema.addEntity("Post");
         post.addLongProperty("id").primaryKey();
@@ -114,6 +114,7 @@ public class TestpressDaoGenerator {
         instituteSettings.addBooleanProperty("requireQuestionReportDescription");
         instituteSettings.addIntProperty("questionReportDescriptionMinLength");
         instituteSettings.addBooleanProperty("qotdEnabled");
+        instituteSettings.addStringProperty("customForgotPasswordUrl");
 
         Entity rssFeed = schema.addEntity("RssItem");
         rssFeed.addLongProperty("id").primaryKey().autoincrement();

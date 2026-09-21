b531d2eac964e4f66014f7cffc2094e694c24e36
Nivethaa-Testpress <nivethaad@testpress.in>
2026-09-21 15:10:52 +0530

    feat: Add messagesEnabled to InstituteSettings and bump greenDAO schema to 49

--- app/src/main/java/in/testpress/testpress/models/InstituteSettings.java
+++ app/src/main/java/in/testpress/testpress/models/InstituteSettings.java
@@ -91,6 +91,7 @@ public class InstituteSettings {
     private Boolean requireQuestionReportDescription;
     private Integer questionReportDescriptionMinLength;
     private Boolean qotdEnabled;
+    private Boolean messagesEnabled;
     private String customForgotPasswordUrl;
 
     // KEEP FIELDS - put your custom fields here
@@ -103,7 +104,7 @@ public class InstituteSettings {
         this.baseUrl = baseUrl;
     }
 
-    public InstituteSettings(String baseUrl, String verificationMethod, Boolean allowSignup, Boolean forceStudentData, Boolean removeTpBranding, String url, Boolean showGameFrontend, Boolean coursesEnabled, Boolean coursesEnableGamification, String coursesLabel, Boolean postsEnabled, String postsLabel, Boolean storeEnabled, String storeLabel, Boolean documentsEnabled, String documentsLabel, Boolean resultsEnabled, Boolean dashboardEnabled, Boolean facebookLoginEnabled, Boolean googleLoginEnabled, boolean commentsVotingEnabled, Boolean bookmarksEnabled, Boolean forumEnabled, String forumLabel, Boolean twilioEnabled, Boolean allow_profile_edit, String learnLabel, String leaderboardLabel, String dashboardLabel, String bookmarksLabel, String loginLabel, String loginPasswordLabel, String aboutUs, Boolean disableStudentAnalytics, Boolean customRegistrationEnabled, Boolean enableParallelLoginRestriction, Integer maxParallelLogins, Integer lockoutLimit, String cooloffTime, String appToolbarLogo, String appShareLink, String serverTime, Boolean allowScreenshotInApp, String androidSentryDns, Boolean leaderboardEnabled, String threatsAndTargetsLabel, Boolean isVideoDownloadEnabled, Boolean isHelpdeskEnabled, IntegerList allowedLoginMethods, Boolean showShareButton, String facebookAppId, Integer maxAllowedDownloadedVideos, Boolean disableForgotPassword, Boolean disableStudentReport, Boolean enableCustomTest, String currentPaymentApp, String customRegistrationUrl, Boolean disableStoreInApp, Boolean salesforceSdkEnabled, String salesforceMcApplicationId, String salesforceMcAccessToken, String salesforceFcmSenderId, String salesforceMarketingCloudUrl, String videoWatermarkType, String videoWatermarkPosition, Boolean useNewDiscountFeat, Boolean requireQuestionReportDescription, Integer questionReportDescriptionMinLength, Boolean qotdEnabled, String customForgotPasswordUrl) {
+    public InstituteSettings(String baseUrl, String verificationMethod, Boolean allowSignup, Boolean forceStudentData, Boolean removeTpBranding, String url, Boolean showGameFrontend, Boolean coursesEnabled, Boolean coursesEnableGamification, String coursesLabel, Boolean postsEnabled, String postsLabel, Boolean storeEnabled, String storeLabel, Boolean documentsEnabled, String documentsLabel, Boolean resultsEnabled, Boolean dashboardEnabled, Boolean facebookLoginEnabled, Boolean googleLoginEnabled, boolean commentsVotingEnabled, Boolean bookmarksEnabled, Boolean forumEnabled, String forumLabel, Boolean twilioEnabled, Boolean allow_profile_edit, String learnLabel, String leaderboardLabel, String dashboardLabel, String bookmarksLabel, String loginLabel, String loginPasswordLabel, String aboutUs, Boolean disableStudentAnalytics, Boolean customRegistrationEnabled, Boolean enableParallelLoginRestriction, Integer maxParallelLogins, Integer lockoutLimit, String cooloffTime, String appToolbarLogo, String appShareLink, String serverTime, Boolean allowScreenshotInApp, String androidSentryDns, Boolean leaderboardEnabled, String threatsAndTargetsLabel, Boolean isVideoDownloadEnabled, Boolean isHelpdeskEnabled, IntegerList allowedLoginMethods, Boolean showShareButton, String facebookAppId, Integer maxAllowedDownloadedVideos, Boolean disableForgotPassword, Boolean disableStudentReport, Boolean enableCustomTest, String currentPaymentApp, String customRegistrationUrl, Boolean disableStoreInApp, Boolean salesforceSdkEnabled, String salesforceMcApplicationId, String salesforceMcAccessToken, String salesforceFcmSenderId, String salesforceMarketingCloudUrl, String videoWatermarkType, String videoWatermarkPosition, Boolean useNewDiscountFeat, Boolean requireQuestionReportDescription, Integer questionReportDescriptionMinLength, Boolean qotdEnabled, Boolean messagesEnabled, String customForgotPasswordUrl) {
         this.baseUrl = baseUrl;
         this.verificationMethod = verificationMethod;
         this.allowSignup = allowSignup;
@@ -173,6 +174,7 @@ public class InstituteSettings {
         this.requireQuestionReportDescription = requireQuestionReportDescription;
         this.questionReportDescriptionMinLength = questionReportDescriptionMinLength;
         this.qotdEnabled = qotdEnabled;
+        this.messagesEnabled = messagesEnabled;
         this.customForgotPasswordUrl = customForgotPasswordUrl;
     }
 
@@ -726,6 +728,14 @@ public class InstituteSettings {
 
     public void setQotdEnabled(Boolean qotdEnabled) {
         this.qotdEnabled = qotdEnabled;
+    }
+
+    public Boolean getMessagesEnabled() {
+        return messagesEnabled;
+    }
+
+    public void setMessagesEnabled(Boolean messagesEnabled) {
+        this.messagesEnabled = messagesEnabled;
     }
 
     public String getCustomForgotPasswordUrl() {

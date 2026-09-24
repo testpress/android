db3c12c801d829789f782bef9d837a28d5d2c21f
Nivethaa-Testpress <nivethaad@testpress.in>
2026-09-24 20:12:51 +0530

    refactor: Remove redundant onOptionsItemSelected override in StudentMessagingActivity

--- app/src/main/java/in/testpress/testpress/ui/StudentMessagingActivity.kt
+++ app/src/main/java/in/testpress/testpress/ui/StudentMessagingActivity.kt
@@ -13,7 +13,6 @@ import `in`.testpress.testpress.models.SsoUrl
 import `in`.testpress.testpress.util.SafeAsyncTask
 import android.net.Uri
 import android.os.Bundle
-import android.view.MenuItem
 import android.view.View
 import android.webkit.WebSettings
 import androidx.appcompat.widget.Toolbar
@@ -115,7 +114,4 @@ class StudentMessagingActivity : TestpressFragmentActivity(), EmptyViewListener 
         fetchSsoLink()
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return super.onOptionsItemSelected(item)
-    }
 }
